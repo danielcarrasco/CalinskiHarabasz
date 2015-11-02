ad880281465bcee1f1c1d0606dda0835c2706299
PabloVillegas <pablovillegaz@gmail.com>
2015-11-01 19:47:28 -0600

    Agregue estas clases y un JFrame de prueba, todavia no paso todo a matrices

--- src/sample/algoritmoCalinskiHarabaz/CalcularAlgoritmoCH.java
+++ src/sample/algoritmoCalinskiHarabaz/CalcularAlgoritmoCH.java
@@ -1,32 +1,25 @@
 package sample.algoritmoCalinskiHarabaz;
 
-import java.util.ArrayList;
-
 /**
  * Created by Villegas on 03/10/2015.
  */
 public class CalcularAlgoritmoCH {
 
-    public double calcularAlgoritmo(ArrayList obj, ArrayList grp, Integer val){
+    public double calcularAlgoritmo(Double[][] objetos, int[][] grupos){
 
-
-        conversiones conv = new conversiones();
+        //Creacion de objetos
+        //conversiones conv = new conversiones();
         centros cen = new centros();
         formarGrupos fg = new formarGrupos();
         distanciaEuclidiana de = new distanciaEuclidiana();
         algoritmosCH ach = new algoritmosCH();
-        quitaJuanito qJ =new quitaJuanito();
-
+        //quitaJuanito qJ =new quitaJuanito();
+        //Creacion Variables
         int nObjetos;
         int nAtributos;
         int nGrupos;
-        Double [][] objetos;
-        conv.conversionObjetos(obj);
-        int [][] grupos;
         double [] cGlobal;
-
         //-----Objetos-------------------
-        objetos = conv.conversionObjetos(obj);
         nObjetos = objetos.length;
         nAtributos = objetos[0].length;
         cGlobal = cen.centros(objetos);
@@ -34,35 +27,40 @@ public class CalcularAlgoritmoCH {
         for(int i = 0; i< cGlobal.length; i++){
             cGlobalT[i] = cGlobal[i];
         }
-
-
         //----Grupos
-        //grupos = conv.conversionGrupos(grupoa);
-
-        grupos = conv.conversionGrupos(qJ.getOutJuanito(grp, val));
         nGrupos = grupos.length;
         double [][] cGrupos = new double[nGrupos][nAtributos];
         //--Formar Grupos
         fg.formarGrupos(cGrupos,grupos, objetos);
-
-
         //------Distancia euclidiana
         double[] distanciasE = new double[nGrupos];
         distanciasE = de.distanciaEuclidianas(distanciasE,grupos,objetos,cGrupos);
-
         //---------Distancia euclidiana Cnetros centros
         double[] distanciasCC = new double [cGrupos.length];
         distanciasCC = de.distanciaECentros(distanciasCC, cGlobalT, cGrupos);
-
-
         //--------Resultado algoritmo
         double aNumerador;
         double aDenominador;
         aNumerador = ach.algoritmoNumerador(nObjetos, nGrupos, grupos, distanciasCC);
         aDenominador = ach.algoritmoDenominador(nGrupos, distanciasE);
 
-
         return (aNumerador/aDenominador);
     }
 
+    public static void main(String[] args) {
+        Double [][] numeros = {{1.0,1.0,0.0},
+                {2.0, 1.0, 1.0},
+                {1.0,2.0,0.0},
+                {2.0,2.0,1.0},
+                {4.0,1.0,3.0},
+                {4.0,2.0,1.0},
+                {4.0,3.0,3.0},
+                {4.0,4.0,5.0},
+                {2.0,4.0,1.0},
+                {3.0,4.0,4.0},
+                {2.0,5.0,2.0}};
+        int [][] grupos = {{0,1,2,3,5,8,10},{4,6,7,9}};
+        CalcularAlgoritmoCH cach = new CalcularAlgoritmoCH();
+        System.out.println(cach.calcularAlgoritmo(numeros,grupos));
+    }
 }
